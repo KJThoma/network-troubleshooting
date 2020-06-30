c51fbc1de3660498cd03d6cfcfc5e6e9e5be250a
KJThoma <49533373+KJThoma@users.noreply.github.com>
2020-06-30 19:43:40 -0400

    Update ping-list.py
    Code cleanup, nicer formatting.

--- ping-list.py
+++ ping-list.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-# This script will check availability of a list of hosts.
+# Ping.py This script will check availability of a list of hosts in the hosts.txt file.
 
 # Module to run the ping command
 import subprocess
 
-# List of IP's to ping
-ip_list = ['192.168.1.254', '192.168.1.240', '192.168.1.9', '8.8.8.8']
+# Empty list where we'll store IPs
+ip_list = []
+
+# Open file with lists of IPs
+with open('hosts.txt', 'r') as hosts_file:
+    for ip in hosts_file:
+        ip_list.append(ip.split('\n')[0])
 
 # Adding some ---- for nicer formatting
-print('-' * 40)
+print('-' * 30)
 
 # For loop to ping objects in List
 # Popen ping sending 5 requests and PIPE to hide stdout for cleaner results
-# wait for the results
+# Wait for the results
 # Poll to check process response and print results, 0 is successful(or UP)
 for ip in ip_list:
     ping = subprocess.Popen(['ping', '-c', '5', ip], stdout=subprocess.PIPE)
-
     ping.wait()
-
     if ping.poll() == 0:
         print(ip + ' is UP')
     else:
         print(ip + ' is DOWN')
 
-print('-' * 40)
+# Adding some ---- for nicer formatting
+print('-' * 30)
+
+# Close file
+hosts_file.close()
